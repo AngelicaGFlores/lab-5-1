37a25aca49ed6bc39687458d42b7b94556dbbf28
AngelicaGFlores <flores.guada.angelica@gmail.com>
2025-09-28 15:00:53 -0700

    yay learning i created the items to display in the cart

---
+++ script.js
@@ -0,0 +1,37 @@
+const productNameInput = document.getElementById("product-name");
+const productPriceInput = document.getElementById("product-price");
+const addProductButton = document.getElementById("add-product");
+const cart = document.getElementById("cart");
+const totalPriceSpan = document.getElementById("total-price");
+
+let totalPrice = 0;
+
+// Function to update the total price
+function updateTotalPrice(amount) {
+	totalPrice += amount;
+	totalPriceSpan.textContent = totalPrice.toFixed(2);
+}
+
+//add product to cart
+// Dynamically add a new product
+addProductButton.addEventListener("click", () => {
+    const nameOfProduct = productNameInput.value;
+    updateTotalPrice(Number(productPriceInput.value));
+
+    
+    //console.log(productNameInput.value);
+	const newProduct = document.createElement("li");
+    // //newProduct.dataset.id = '3';
+
+    newProduct.innerHTML = `${nameOfProduct} $${productPriceInput.value}  <button class="Removed">Remove</button>`;
+
+	cart.appendChild(newProduct);
+});
+
+// Function to remove an item
+function removeItem(event) {
+	const item = event.target.closest("li");
+	const price = parseFloat(item.dataset.price);
+	updateTotalPrice(-price);
+	item.remove();
+}
